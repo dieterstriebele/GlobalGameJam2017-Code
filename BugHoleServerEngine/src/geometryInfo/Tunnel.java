4005f18ddd230a93350495bdb96c69b5349fa0db
Andreas Dieling <adieling@gmx.de>
2017-01-21 22:57:49 +0100

    Added back and forwards tunnel segment for continues flight

--- BugHoleServerEngine/src/geometryInfo/Tunnel.java
+++ BugHoleServerEngine/src/geometryInfo/Tunnel.java
@@ -9,11 +9,13 @@ import java.nio.file.Paths;
 import game.Settings;
 import util.Logger;
 
+import static java.lang.Math.abs;
+
 public class Tunnel extends GeometryInformationBase {
 
 	private float m_Time;
 
-	private static final int NumberOfCameraSegments = 1;
+	private static final int NumberOfCameraSegments = 3;
 	
 	public float m_IntestineScrollingOffset = 0f;
 	
@@ -30,13 +32,17 @@ public class Tunnel extends GeometryInformationBase {
 			_rotations.get(i).mYPos = 1.570796f;
 		}
 		
-		m_tunnel_positions = _LoadTunnelPathFromFile(Paths.get("res/intestines_triplepath_001_kbap.bin").toAbsolutePath().toString());		
+		m_tunnel_positions = _LoadTunnelPathFromFile(Paths.get("res/intestines_triplepath_001_kbap.bin").toAbsolutePath().toString());
+
+//        float x_d = abs(m_tunnel_positions[0] - m_tunnel_positions[m_tunnel_positions.length-3]);
+//        float y_d = abs(m_tunnel_positions[2] - m_tunnel_positions[m_tunnel_positions.length-1]);
+//        float z_d = abs(m_tunnel_positions[1] - m_tunnel_positions[m_tunnel_positions.length-2]);
 	}
-	
+
 	private float[] _LoadTunnelPathFromFile(String inputPath)
 	{
 		float[] positions = null;
-		
+
 		try {
 			DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(inputPath)));
 			
@@ -71,7 +77,7 @@ public class Tunnel extends GeometryInformationBase {
 
 		//Logger.Info("In SynchronizeState of Tunnel");
 
-		m_Time += 8.00f;
+		m_Time += 16.00f;
 		int i=0;
 
 		int m_PathSegmentOffset_01 = (int)m_Time*1 % (m_tunnel_positions.length/3);
@@ -86,6 +92,35 @@ public class Tunnel extends GeometryInformationBase {
 		_scaling.get(i).mXPos = 1.0f;
 		_scaling.get(i).mYPos = 1.0f;
 		_scaling.get(i).mZPos = 1.0f;
+		i++;
+
+        int m_PathSegmentOffset_02 = (int)m_Time*1 % (m_tunnel_positions.length/3);
+        m_PathSegmentOffset_02 *= 3;
+        _positions.get(i).mXPos = m_tunnel_positions[m_PathSegmentOffset_02 + 0];
+        _positions.get(i).mYPos = m_tunnel_positions[m_PathSegmentOffset_02 + 2];
+        _positions.get(i).mZPos = -m_tunnel_positions[m_PathSegmentOffset_02 + 1] + 100.0f;
+        _rotations.get(i).mXPos = 0.0f;
+        _rotations.get(i).mYPos = 0.0f;
+        _rotations.get(i).mZPos = 0.0f;
+        _scaling.get(i).mXPos = 1.0f;
+        _scaling.get(i).mYPos = 1.0f;
+        _scaling.get(i).mZPos = 1.0f;
+        i++;
+
+        int m_PathSegmentOffset_03 = (int)m_Time*1 % (m_tunnel_positions.length/3);
+        m_PathSegmentOffset_03 *= 3;
+        _positions.get(i).mXPos = m_tunnel_positions[m_PathSegmentOffset_03 + 0];
+        _positions.get(i).mYPos = m_tunnel_positions[m_PathSegmentOffset_03 + 2];
+        _positions.get(i).mZPos = -m_tunnel_positions[m_PathSegmentOffset_03 + 1] - 100.0f;
+        _rotations.get(i).mXPos = 0.0f;
+        _rotations.get(i).mYPos = 0.0f;
+        _rotations.get(i).mZPos = 0.0f;
+        _scaling.get(i).mXPos = 1.0f;
+        _scaling.get(i).mYPos = 1.0f;
+        _scaling.get(i).mZPos = 1.0f;
+        i++;
+
+
 //		_graphicsId = IGeometryInformation.cOBJECTMODELIDENTIFICATION_INTESTINES_SIMPLE;
 //		_numObjects = 1;
 
